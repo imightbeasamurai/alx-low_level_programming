e270f52120ff5236fc659c2f53630d225c81a942
yamurai <jsamin3@gmail.com>
2023-06-16 11:25:50 -0700

    morecc

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit nums
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		putchar(n);
+		n++;
+	}
+	return (0);
+}
