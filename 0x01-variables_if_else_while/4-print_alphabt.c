2eff1460e8a5816c24e09013b4a857a21819baa6
yamurai <jsamin3@gmail.com>
2023-06-16 11:22:22 -0700

    morecc

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets without e and q
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	a = 97;
+	while (a < 123)
+	{
+		if (a == 101)
+			a = 102;
+		else if (a == 113)
+			a = 114;
+		putchar(a);
+		a++;
+	}
+	return (0);
+}
