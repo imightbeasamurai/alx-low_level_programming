0419ff302e9fd84b5b7d3d863e4e736f0a4f6281
yamurai <jsamin3@gmail.com>
2023-06-16 11:44:10 -0700

    morecc

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print base 16
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int a;
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+	for (ch = 'a'; a <= 'f'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
