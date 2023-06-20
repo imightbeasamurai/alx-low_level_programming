296060f02cfc8590175fe8a1e55571ca1eb260e9
yamurai <jsamin3@gmail.com>
2023-06-20 02:24:24 -0700

    autopush

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints a char by char with a loop
+ * Return: 0
+ */
+int main()
+{
+	int len;
+	char quote[9];
+	int index;
+
+	index = 0;
+	quote = "_putchar"
+	len = strlen(quote);
+	while (i < len)
+		putchar(quote[i]);
+	return (0);
+}
