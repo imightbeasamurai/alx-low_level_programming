d75814a25a02d6d650af4fa5216fc36f11a634a8
yamurai <jsamin3@gmail.com>
2023-06-20 02:35:33 -0700

    autopush

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print alphabets
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int index;
+
+	for (index = 'a'; index <= 'z'; index++)
+
+	{
+		_putchar(index);
+	}
+	_putchar('\n');
+}
