53c35e85584a6a48e0c8252121acb0460c3fc636
yamurai <jsamin3@gmail.com>
2023-07-27 03:23:41 -0700

    autopush

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t i;
 
-	nelem = 0;
-	while (h != NULL)
+	i = 0;
+	while (h)
 	{
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nelem++;
+		i++;
 	}
-	return (nelem);
+	return (i);
 }
