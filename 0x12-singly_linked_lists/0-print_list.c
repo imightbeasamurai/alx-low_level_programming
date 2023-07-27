e36dafd4bf7278426aadb5433596868f7c2fb332
yamurai <jsamin3@gmail.com>
2023-07-27 03:22:39 -0700

    autopush

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list
+ * print_list - prints all the elements of a list.
  * @h: const list_t *h
  * Return: size_t
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t nelem;
 
-	i = 0;
+	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		i++;
+		nelem++;
 	}
-	return (i);
+	return (nelem);
 }
